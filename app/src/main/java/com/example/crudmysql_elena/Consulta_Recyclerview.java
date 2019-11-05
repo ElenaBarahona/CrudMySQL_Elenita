3dbbd313fbe8e593a440c254b4cc33c1a9a508c8
ElenaBarahona <elenabarahona145@gmail.com>
2019-11-05 16:22:38 -0600

    Agrege la clase Consulta_Recyclerview

---
+++ app/src/main/java/com/example/crudmysql_elena/Consulta_Recyclerview.java
@@ -0,0 +1,14 @@
+package com.example.crudmysql_elena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Consulta_Recyclerview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta__recyclerview);
+    }
+}
