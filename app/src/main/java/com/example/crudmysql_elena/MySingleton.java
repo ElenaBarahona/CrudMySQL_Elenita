9a481ba1ab59f9d88004105fd9ea6b7c4fdc1947
ElenaBarahona <elenabarahona145@gmail.com>
2019-11-06 08:31:00 -0600

    adicione cambios

--- app/src/main/java/com/example/crudmysql_elena/MySingleton.java
+++ app/src/main/java/com/example/crudmysql_elena/MySingleton.java
@@ -5,57 +5,60 @@ import android.graphics.Bitmap;
 
 import androidx.collection.LruCache;
 
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 
 public class MySingleton {
-    private static MySingleton mInstance;
-    private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
-    private static Context mCtx;
 
-    private MySingleton(Context context) {
-        mCtx = context;
-        mRequestQueue = getRequestQueue();
+        private static MySingleton mInstance;
+        private RequestQueue mRequestQueue;
+        private ImageLoader mImageLoader;
+        private static Context mCtx;
 
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
+        private MySingleton(Context context) {
+            mCtx = context;
+            mRequestQueue = getRequestQueue();
 
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
+            mImageLoader = new ImageLoader(mRequestQueue,
+                    new ImageLoader.ImageCache() {
+                        private final LruCache<String, Bitmap>
+                                cache = new LruCache<String, Bitmap>(20);
 
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
-    }
+                        @Override
+                        public Bitmap getBitmap(String url) {
+                            return cache.get(url);
+                        }
 
-    public static synchronized MySingleton getInstance(Context context) {
-        if (mInstance == null) {
-            mInstance = new MySingleton(context);
+                        @Override
+                        public void putBitmap(String url, Bitmap bitmap) {
+                            cache.put(url, bitmap);
+                        }
+                    });
         }
-        return mInstance;
-    }
 
-    public RequestQueue getRequestQueue() {
-        if (mRequestQueue == null) {
-
-            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        public static synchronized MySingleton getInstance(Context context) {
+            if (mInstance == null) {
+                mInstance = new MySingleton(context);
+            }
+            return mInstance;
         }
-        return mRequestQueue;
+
+        public RequestQueue getRequestQueue() {
+            if (mRequestQueue == null) {
+
+                mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+            }
+            return mRequestQueue;
+        }
+
+        public <T> void addToRequestQueue(Request<T> req) {
+            getRequestQueue().add(req);
+        }
+
+        public ImageLoader getImageLoader() {
+            return mImageLoader;
+        }
+
     }
-
-    public <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
-    }
-
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
-    }
-
-}
-
