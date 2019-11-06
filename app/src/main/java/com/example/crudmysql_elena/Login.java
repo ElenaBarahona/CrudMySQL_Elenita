b37453f5e921648650b204454b990c4e6047c4fc
ElenaBarahona <elenabarahona145@gmail.com>
2019-11-06 08:29:12 -0600

    Agrege la clase Login

--- app/src/main/java/com/example/crudmysql_elena/Login.java
+++ app/src/main/java/com/example/crudmysql_elena/Login.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.CrudMySQL_Elena.R;
+
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+         setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_login);
     }
 }
